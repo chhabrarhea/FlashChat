f161a099f61ef758d883974963f10470e100bc31
Rhea <rheachhabra1011@gmail.com>
2020-11-08 12:43:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/flashchat/LoginActivity.kt
@@ -0,0 +1,37 @@
+package com.example.flashchat
+
+import android.R.attr.phoneNumber
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.flashchat.databinding.ActivityLoginBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.PhoneAuthOptions
+import com.google.firebase.auth.PhoneAuthProvider
+import com.hbb20.CountryCodePicker
+import java.util.concurrent.TimeUnit
+import android.content.Intent
+
+
+class LoginActivity : AppCompatActivity() {
+    lateinit var binding:ActivityLoginBinding
+    lateinit var code:CountryCodePicker
+    lateinit var number:EditText
+    lateinit var auth:FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding=DataBindingUtil.setContentView(this, R.layout.activity_login)
+        code=binding.code
+        number=binding.number
+        code.registerCarrierNumberEditText(number)
+        auth= FirebaseAuth.getInstance()
+    }
+    fun login(view: View) {
+        var number: String = code.fullNumber
+        var intent=Intent(applicationContext,OtpActivity::class.java)
+        intent.putExtra("num",number)
+        startActivity(intent)
+    }
+}
---
+++ app/src/main/java/com/example/flashchat/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.flashchat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.DataBindingUtil.setContentView
+import com.example.flashchat.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding:ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+
+    fun login(view:View){
+        
+    }
+    fun register(view:View){
+
+    }
+}
